53bb8da7a8d3247e14e043515a39b1530152e9d9
martinMarkov13 <markow9305@gmail.com>
2023-02-10 13:35:17 +0200

    Create 07. Pet shop.js

---
+++ Programming Basics with JavaScript/01. First-Steps-In-Programming/Lab/07. Pet shop.js
@@ -0,0 +1,7 @@
+function petShop(input){
+    let dogFood = Number(input[0]);
+    let catFood = Number(input[1]);
+    let suma = (dogFood*2.50) + (catFood*4);
+console.log(`${suma} lv`);
+}
+petShop(["2","3"])
