dc1fcab491bc0e9c76548f618abb0b430148f225
martinMarkov13 <markow9305@gmail.com>
2023-02-13 13:25:42 +0200

    Problem 5

---
+++ Programming Basics with JavaScript/02. Conditional Statements/Lab/05. Number100...200.js
@@ -0,0 +1,14 @@
+function Number100200(input){
+    let chislo = Number (input[0])
+    if (chislo <100){
+        console.log("Less than 100")
+    }
+    else if(chislo >200){
+        console.log("Greater than 200")
+    } else if (chislo >= 100){
+        console.log("Between 100 and 200")
+    }
+    
+    
+    }
+Number100200(["12"])
