3ef9f77c01e363d15c33355686db60c97dd078df
martinMarkov13 <markow9305@gmail.com>
2023-02-14 12:23:47 +0200

    Problem 2

---
+++ Programming Basics with JavaScript/02. Conditional Statements/Exercise/02. bonusScore.js
@@ -0,0 +1,20 @@
+function bonusScore(input){
+    let number = Number(input[0])
+    let bonus = 0.0
+
+    if(number<=100){
+        bonus = 5
+    } else if(number<1000){
+        bonus = number * 0.2
+    } else if(number>1000){
+        bonus = number * 0.1
+    }
+    if(number%2===0){
+        bonus += 1 
+    } else if(number % 10 ===5){
+        bonus += 2
+    }
+    console.log(bonus)
+    console.log(number+bonus)
+}
+bonusScore(["15875"])
