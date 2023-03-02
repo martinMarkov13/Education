e5c2b98f606f0b3264b5f4a06304245697b45bbb
martinMarkov13 <markow9305@gmail.com>
2023-03-02 23:35:04 +0200

    Create 09. Loading Bar.js

---
+++ JavaScript Fundamentals/04. Functions/09. Loading Bar.js
@@ -0,0 +1,14 @@
+function loadingBar(number) {
+  let counter = number / 10;
+  let procentages = "%".repeat(counter);
+  let dots = ".".repeat(10 - counter);
+
+  if (number === 100) {
+    console.log(`${number}% Complete!`);
+    console.log(`[${procentages}]`);
+  } else {
+    console.log(`${number}% [${procentages}${dots}]`);
+    console.log(`Still loading...`);
+  }
+}
+loadingBar(50);
