2089e7c337393c572fe2e42971e315c817fbed26
martinMarkov13 <markow9305@gmail.com>
2023-03-07 19:17:08 +0200

    Create 03. Heart Delivery.js

---
+++ JavaScript Fundamentals/06. Mid exam/03. Heart Delivery.js
@@ -0,0 +1,34 @@
+function solve(input) {
+  let houses = input.shift().split("@").map(Number);
+  let cupid = 0;
+
+  while (input[0] !== "Love!") {
+    let tokens = input.shift().split(" ");
+    let jump = Number(tokens[1]);
+    cupid += jump;
+    if (cupid >= houses.length) {
+      cupid = 0;
+    }
+    if (houses[cupid] == 0) {
+      console.log(`Place ${cupid} already had Valentine's day.`);
+    } else {
+      houses[cupid] -= 2;
+      if (houses[cupid] == 0) {
+        console.log(`Place ${cupid} has Valentine's day.`);
+      }
+    }
+  }
+  let missed = 0;
+  for (let house of houses) {
+    if (house > 0) {
+      missed++;
+    }
+  }
+  console.log(`Cupid's last position was ${cupid}.`);
+  if (missed > 0) {
+    console.log(`Cupid has failed ${missed} places.`);
+  } else {
+    console.log(`Mission was successful.`);
+  }
+}
+solve(["10@10@10@2", "Jump 1", "Jump 2", "Love!"]);
