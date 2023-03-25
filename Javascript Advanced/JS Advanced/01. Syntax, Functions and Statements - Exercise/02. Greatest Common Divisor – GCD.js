9d83930fb8579532f14348eface131c69b2ffaf4
martinMarkov13 <markow9305@gmail.com>
2023-03-25 20:39:41 +0200

    Create 02. Greatest Common Divisor – GCD.js

---
+++ Javascript Advanced/JS Advanced/01. Syntax, Functions and Statements - Exercise/02. Greatest Common Divisor – GCD.js
@@ -0,0 +1,9 @@
+function solve(number1, number2) {
+    for (let i = Math.min(number1, number2); i >=1; i--){
+        if (number1 % i === 0 && number2 % i === 0){
+            console.log(i)
+            break
+        }
+    }
+}
+solve(15,5)
