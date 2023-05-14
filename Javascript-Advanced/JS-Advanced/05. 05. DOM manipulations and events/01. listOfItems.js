9279d71c9618041b011d2969cfd4394c22fb4569
martinMarkov13 <markow9305@gmail.com>
2023-05-14 18:37:03 +0300

    Create 01. listOfItems.js

---
+++ Javascript-Advanced/JS-Advanced/05. 05. DOM manipulations and events/01. listOfItems.js
@@ -0,0 +1,8 @@
+function addItem() {
+    let input = document.getElementById("newItemText").value;
+    let list = document.getElementById("items");
+    let li=document.createElement("li");
+    li.textContent = input;
+    list.appendChild(li);
+    document.getElementById("newItemText").value = "";
+}
