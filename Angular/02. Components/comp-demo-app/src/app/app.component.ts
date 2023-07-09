c60f726b94bfc2ac93628935e9db22c2ef016753
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2023-07-09 13:02:20 +0300

    Components test demo

---
+++ Angular/02. Components/comp-demo-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  titleFromParent = 'title from Parent - AppComponent';
+
+  users = [
+    { name: 'Mitko', age: 21 },
+    { name: 'Pesho', age: 22 },
+    { name: 'Mariya', age: 43 },
+    { name: 'Ivan', age: 16 },
+  ];
+
+  onOutputFromChild() {
+    console.log('on output from child');
+  }
+}
---
+++ Angular/02. Components/comp-demo-app/src/app/navigation/navigation.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css'],
+})
+export class NavigationComponent implements OnInit {
+  @Input('title') titleFromApp: string = '';
+  @Input('activeUsers') activeUsers: {name:string, age:number}[] = [];
+  @Output() onChildOutput= new EventEmitter<boolean>()
+
+  isActive = false;
+  inputValue = 'Hello!';
+
+  ngOnInit(): void {
+    console.log('navigation init');
+  }
+
+  // activeUsers = [
+  //   { name: 'Mitko', age: 21 },
+  //   { name: 'Pesho', age: 22 },
+  //   { name: 'Mariya', age: 43 },
+  //   { name: 'Ivan', age: 16 },
+  // ];
+
+  handleClick(): void {
+    this.isActive = !this.isActive;
+    this.onChildOutput.emit(this.isActive)
+  }
+}
