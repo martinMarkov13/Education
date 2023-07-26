733956c3039ebd12ce3cf2d49c74a9cf07614940
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2023-07-26 23:43:46 +0300

    Modules and routing test

---
+++ Angular/05.Modules-and-routing/modules/src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from 'src/app/types/User';
+import { GlobalLoaderService } from 'src/app/core/global-loader/global-loader.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+})
+export class UserListComponent implements OnInit {
+  userList: User[] = [];
+
+  constructor(private userService: UserService, private globalLoaderService:GlobalLoaderService) {}
+
+  ngOnInit(): void {
+    this.globalLoaderService.showLoader();
+
+    // setTimeout(() => {
+      this.userService.fetchUsers().subscribe((users) => {
+        this.userList = users;
+        this.globalLoaderService.hideLoader();
+      });
+    // }, 2000);
+
+  }
+}
