fd6d78b8d825e34802c0de76fc39737fed0cbb35
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2023-08-17 23:50:53 +0300

    controlled forms demo

--- ReactJS/05. Forms/react-forms-demo/src/baseControlledForms.js
+++ ReactJS/05. Forms/react-forms-demo/src/baseControlledForms.js
@@ -1,24 +1,55 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [hobbies,  setHobbies ] = useState({})
-  const [values, setValues] = useState({
-    username: 'Pesho',
-    creditCard: "",
-    occupation: 'engineering',
-    gender: 'male',
-    bio: "",
-    age: "",
-  })
+  const [username, setUsername] = useState("Pesho");
+  const [creditCard, setCreditCard] = useState("");
+  const [occupation, setOccupation] = useState("engineering")
+  const [gender, setGender] = useState('male')
+  const [bio, setBio] = useState("")
 
-  const onChangeHandler = (e) => {
-    setValues(state => ({...state, [e.target.name]: e.target.value}))
-  }
+  const [age, setAge] = useState();
+  const [hobbies,  setHobbies ] = useState({})
+
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setUsername("Gosho");
+    }, 2000);
+  }, []);
+
+  const onUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onAgeChange = (e) => {
+    setAge(Number(e.target.value));
+  };
+
+  const onCreditCardChange = (e) => {
+    setCreditCard(e.target.value);
+  };
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    console.log(username);
+    console.log(age);
+    console.log(creditCard);
   };
+  
+  const onOccupationChange = (e) => {
+    setOccupation(e.target.value)
+    console.log(e.target.value);
+  }
+
+  const onGenderChange = (e) => {
+    setGender(e.target.value)
+  }
+
+  const onBioChange = (e) => {
+    setBio(e.target.value)
+  }
 
   const onHobbiesChange = (e) => {
     console.log(e.target.value);
@@ -26,20 +57,18 @@ function App() {
     setHobbies( state => ({...state, [e.target.value]:e.target.checked}))    
   }
 
-
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={onSubmitHandler}>
-
           <div>
             <label htmlFor="username">Username</label>
             <input
               type="text"
               name="username"
               id="username"
-              value={values.username}
-              onChange={onChangeHandler}
+              value={username}
+              onChange={onUsernameChange}
             />
           </div>
 
@@ -49,26 +78,26 @@ function App() {
               type="number"
               name="age"
               id="age"
-              value={values.age}
-              onChange={onChangeHandler}
+              value={age ?? ""}
+              onChange={onAgeChange}
             />
           </div>
-          {Number(values.age) >= 18 && (
+          {age >= 18 && (
             <div>
               <label htmlFor="credit-card">Credit Card</label>
               <input
                 type="text"
                 name="creditCard"
                 id="credit-card"
-                value={values.creditCard}
-                onChange={onChangeHandler}
+                value={creditCard}
+                onChange={onCreditCardChange}
               />
             </div>
           )}
 
           <div>
             <label htmlFor="occupation">Occupation</label>
-            <select name="occupation" id="occupation" value={values.occupation} onChange={onChangeHandler}>
+            <select name="occupation" id="occupation" value={occupation} onChange={onOccupationChange}>
               <option value="it">IT</option>
               <option value="medicine">Medicine</option>
               <option value="engineering">Engineering</option>
@@ -77,14 +106,14 @@ function App() {
 
           <div>
             <label htmlFor="female">Gender</label>
-            <input type="radio" name="gender" id="female"  value="female" onChange={onChangeHandler} checked={values.gender === 'female'}/>
+            <input type="radio" name="gender" id="female"  value="female" onChange={onGenderChange} checked={gender === 'female'}/>
             <label htmlFor="male">Male</label>
-            <input type="radio" name="gender" id="male"  value="male" onChange={onChangeHandler} checked={values.gender === 'male'}/>
+            <input type="radio" name="gender" id="male"  value="male" onChange={onGenderChange} checked={gender === 'male'}/>
           </div>
 
           <div>
             <label htmlFor="bio">Bio</label>
-            <textarea name="bio" id="bio" cols="30" rows="10" value={values.bio} onChange={onChangeHandler}></textarea>
+            <textarea name="bio" id="bio" cols="30" rows="10" value={bio} onChange={onBioChange}></textarea>
           </div>
 
           <div>
