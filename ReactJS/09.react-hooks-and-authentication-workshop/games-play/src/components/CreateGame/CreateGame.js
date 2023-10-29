0d81d6bcf9af4047052d78eb945520f1f678cab3
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2023-10-29 22:03:38 +0200

    a
    a

--- ReactJS/09.react-hooks-and-authentication-workshop/games-play/src/components/CreateGame/CreateGame.js
+++ ReactJS/09.react-hooks-and-authentication-workshop/games-play/src/components/CreateGame/CreateGame.js
@@ -1,49 +1,83 @@
-import { useForm } from "../../hooks/useForm"
+import { useForm } from "../../hooks/useForm";
 
-export function CreateGame({
-    onCreateGameSubmitHandler,
-}){
+export function CreateGame({ onCreateGameSubmitHandler }) {
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      title: "",
+      category: "",
+      maxLevel: "",
+      imageUrl: "",
+      summary: "",
+    },
+    onCreateGameSubmitHandler
+  );
 
-    const {values, changeHandler, onSubmit} = useForm({
-        title:'',
-        category: '',
-        maxLevel: '',
-        imageUrl: '',
-        summary: '',
-    }, onCreateGameSubmitHandler)
+  // function onChangeHandler(e){
+  //     setValues(state=> ({...state, [e.target.name]:e.target.value}))
+  // }
 
-    // function onChangeHandler(e){
-    //     setValues(state=> ({...state, [e.target.name]:e.target.value}))
-    // }
+  // const onSubmit = (e) => {
+  //     e.preventDefault()
+  //     onCreateGameSubmitHandler(values)
+  // }
 
-    // const onSubmit = (e) => {
-    //     e.preventDefault()
-    //     onCreateGameSubmitHandler(values)
-    // } 
+  return (
+    <section id="create-page" className="auth">
+      <form id="create" method="POST" onSubmit={onSubmit}>
+        <div className="container">
+          <h1>Create Game</h1>
 
-    return (
-        <section id="create-page" className="auth">
-        <form id="create" method="POST" onSubmit={onSubmit}>
-            <div className="container">
-                <h1>Create Game</h1>
+          <label htmlFor="leg-title">Legendary title:</label>
+          <input
+            value={values.title}
+            onChange={changeHandler}
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Enter game title..."
+          />
 
-                <label htmlFor="leg-title">Legendary title:</label>
-                <input value={values.title} onChange={changeHandler}  type="text" id="title" name="title" placeholder="Enter game title..."/>
+          <label htmlFor="category">Category:</label>
+          <input
+            value={values.category}
+            onChange={changeHandler}
+            type="text"
+            id="category"
+            name="category"
+            placeholder="Enter game category..."
+          />
 
-                <label htmlFor="category">Category:</label>
-                <input value={values.category} onChange={changeHandler} type="text" id="category" name="category" placeholder="Enter game category..."/>
+          <label htmlFor="levels">MaxLevel:</label>
+          <input
+            value={values.maxLevel}
+            onChange={changeHandler}
+            type="number"
+            id="maxLevel"
+            name="maxLevel"
+            min="1"
+            placeholder="1"
+          />
 
-                <label htmlFor="levels">MaxLevel:</label>
-                <input value={values.maxLevel} onChange={changeHandler}  type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1"/>
+          <label htmlFor="game-img">Image:</label>
+          <input
+            value={values.imageUrl}
+            onChange={changeHandler}
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            placeholder="Upload a photo..."
+          />
 
-                <label htmlFor="game-img">Image:</label>
-                <input value={values.imageUrl} onChange={changeHandler} type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..."/>
-
-                <label htmlFor="summary">Summary:</label>
-                <textarea name="summary" id="summary" value={values.summary} onChange={changeHandler}></textarea>
-                <input className="btn submit" type="submit" value="Create Game"/>
-            </div>
-        </form>
+          <label htmlFor="summary">Summary:</label>
+          <textarea
+            name="summary"
+            id="summary"
+            value={values.summary}
+            onChange={changeHandler}
+          ></textarea>
+          <input className="btn submit" type="submit" value="Create Game" />
+        </div>
+      </form>
     </section>
-    )
+  );
 }
