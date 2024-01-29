3818b1f5869dc4689b53c755a3655cc1c3ab73b2
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-01-29 16:39:45 +0200

    Add handlebars and index page

--- JS BackEnd/cubicle-workshop-express-and-templating/src/index.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/index.js
@@ -1,1 +1,19 @@
-console.log("hello from express");
+const express = require("express");
+const handlebars = require("express-handlebars")
+
+const app = express();
+const PORT = 5000;
+
+// Handlebars config
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+// Routes
+app.get("/", (req, res) => {
+  res.render('index')
+});
+
+app.listen(PORT, () => console.log("Server is running on port 5000..."));
