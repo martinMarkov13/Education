9193c3715908f1af6031196593a83a597a0bf0ee
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-10 16:27:20 +0200

    Setup mongoose and DB

--- JS BackEnd/cubicle-workshop-express-and-templating/src/index.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 
-const expressConfig = require("./config/expressConfig")
-const handlebarsConfig = require('./config/handlebarsConfig')
-const routes = require('./routes')
+const expressConfig = require("./config/expressConfig");
+const handlebarsConfig = require("./config/handlebarsConfig");
+const routes = require("./routes");
+const dbConnect = require("./config/dbConfig");
 
 const app = express();
 const PORT = 5000;
 
-expressConfig(app)
-handlebarsConfig(app)
-app.use(routes)
+expressConfig(app);
+handlebarsConfig(app);
+
+dbConnect()
+  .then(() => console.log(`DB Connected succesfully`))
+  .catch((err) => console.log(("DB Error:", err)));
+
+app.use(routes);
 
 app.listen(PORT, () => console.log("Server is running on port 5000..."));
