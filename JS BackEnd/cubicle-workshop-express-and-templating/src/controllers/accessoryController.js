a2ec5a0b4428c87db17734144515c185819edab8
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-10 18:21:56 +0200

    add accessory to DB

--- JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/accessoryController.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/accessoryController.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
 
+const accessoryService = require("../services/accessoryService");
+
 router.get("/create", (req, res) => {
   res.render("accessory/create");
 });
 
-router.post("/create", (req, res) => {
-    const body = req.body;
+router.post("/create", async (req, res) => {
+  const { name, description, imageUrl } = req.body;
 
-    res.redirect('/')
-})
+  await accessoryService.create({ name, description, imageUrl });
+
+  res.redirect("/");
+});
 
 module.exports = router;
--- JS BackEnd/cubicle-workshop-express-and-templating/src/services/accessoryService.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/services/accessoryService.js
@@ -1,6 +1,3 @@
+const Accessory = require('../models/Accessory')
 
-
-
-exports.create = (accessoryData) => {
-    console.log(accessoryData);
-}
+exports.create = (accessoryData) => Accessory.create(accessoryData)
