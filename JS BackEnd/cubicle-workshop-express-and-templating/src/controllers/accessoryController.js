075ae1d3406c26ab806f48583dd1a5ddf926b12c
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-10 17:59:06 +0200

    add missing slash

--- JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/accessoryController.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/accessoryController.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/create", (req, res) => {
-  res.render("accessory/createAccessory");
+  res.render("accessory/create");
 });
 
 module.exports = router;
