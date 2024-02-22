fccb90058b68b1543dd0d09d6e0fa63351f200e9
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-22 22:32:33 +0200

    hash password before save

--- JS BackEnd/cubicle-workshop-express-and-templating/src/models/User.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/models/User.js
@@ -1,24 +1,22 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: String,
-  password: {
-    type: String
-  //   validate: {
-  //     validator: function (value) {
-  //       return this.repeatPassword === value;
-  //     },
-  //     message: `Passwords missmatch!`
-  //   },
-  },
-})
+  password: String,
+});
 
-userSchema.virtual('repeatPassword')
-  .set(function(value){
-    if (value !== this.password){
-      throw new mongoose.MongooseError("Password mismatch!")
-    }
-  })
+userSchema.virtual("repeatPassword").set(function (value) {
+  if (value !== this.password) {
+    throw new mongoose.MongooseError("Password mismatch!");
+  }
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+});
 
 const User = mongoose.model("User", userSchema);
 
