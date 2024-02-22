18968e1a179b5d902651e202cbca29a49a9af652
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-22 22:05:20 +0200

    Save user with validate from DB

--- JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/userController.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/userController.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
+const userService = require("../services/userService");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
 
+  await userService.register({username, password, repeatPassword});
+
+  res.redirect("/users/login");
+});
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
 
 module.exports = router;
