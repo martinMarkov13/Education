170bc4f2c786952cd33ae3bde643eb182078735e
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-23 23:03:52 +0200

    add delete cube page

--- JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/cubeController.js
+++ JS BackEnd/cubicle-workshop-express-and-templating/src/controllers/cubeController.js
@@ -5,7 +5,7 @@ const accessoryService = require("../services/accessoryService");
 
 // Path /cubes/create but it's fixed in index.js
 router.get("/create", (req, res) => {
-  res.render("create");
+  res.render("cube/create");
 });
 
 router.post("/create", async (req, res) => {
@@ -22,31 +22,41 @@ router.post("/create", async (req, res) => {
 });
 
 router.get("/:cubeId/details", async (req, res) => {
-  const cube = await cubeService.getOneWithAccessories(req.params.cubeId).lean();
+  const cube = await cubeService
+    .getOneWithAccessories(req.params.cubeId)
+    .lean();
 
   if (!cube) {
     return res.redirect("/404");
   }
 
-  res.render("details", {cube});
+  res.render("cube/details", { cube });
 });
 
 router.get("/:cubeId/attach-accessory", async (req, res) => {
   const cube = await cubeService.getOne(req.params.cubeId).lean();
-  const accessories = await accessoryService.getNotAttached(cube.accessories).lean();
+  const accessories = await accessoryService
+    .getNotAttached(cube.accessories)
+    .lean();
 
- const hasAccessories = accessories.length > 0;
+  const hasAccessories = accessories.length > 0;
 
   res.render("accessory/attach", { cube, accessories, hasAccessories });
 });
 
-router.post('/:cubeId/attach-accessory', async (req, res)=>{
+router.post("/:cubeId/attach-accessory", async (req, res) => {
   const { accessory } = req.body;
 
-  const cubeId = req.params.cubeId
+  const cubeId = req.params.cubeId;
 
-  await cubeService.attachAccessory(cubeId, accessory)
-  res.redirect(`/cubes/${cubeId}/details`)
-})
+  await cubeService.attachAccessory(cubeId, accessory);
+  res.redirect(`/cubes/${cubeId}/details`);
+});
+
+router.get("/:cubeId/delete", async (req, res) => {
+ const cube = await cubeService.getOne(req.params.cubeId).lean()
+ 
+  res.render("cube/delete", {cube});
+});
 
 module.exports = router;
