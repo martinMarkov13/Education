ea8db47f5f9c2aad624243cc924c09afa693138b
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-29 22:39:18 +0200

    render login, register page and User model

--- JS BackEnd/exam-preparation-skeleton/src/routes.js
+++ JS BackEnd/exam-preparation-skeleton/src/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const homeController = require('./controller/homeController')
+const userController = require('./controller/userController')
 
 router.use(homeController)
+router.use('/users', userController)
 
 module.exports = router;
