531a26fc5ebb681d9596767037408c47d289dbe4
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-29 22:53:15 +0200

    add userService and register

--- JS BackEnd/exam-preparation-skeleton/src/controller/userController.js
+++ JS BackEnd/exam-preparation-skeleton/src/controller/userController.js
@@ -1,11 +1,24 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const  userService  = require("../services/userService");
 
-router.get('/login', (req, res) => {
-    res.render('users/login')
-})
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
 
-router.get('/register', (req, res) => {
-    res.render('users/register')
-})
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+router.post("/register", async (req, res) => {
+  const userData = req.body;
+
+  try{
+      await userService.register(userData);
+      res.redirect('/users/login')
+
+  }catch(err){
+    console.log(err.message);
+  }
+});
 
 module.exports = router;
--- JS BackEnd/exam-preparation-skeleton/src/models/User.js
+++ JS BackEnd/exam-preparation-skeleton/src/models/User.js
@@ -1,21 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'Username is required!'],
-        unique: [true, 'Username already exists!']
-    },
-    email: {
-        type: String,
-        required: [true, "Email is required!"],
-    },
-    password:{
-        type: String,
-        required: [true, "Password is required!"]
-    }
-})
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    unique: [true, "Username already exists!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required!"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required!"],
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.virtual('repeatPassword')
+.set(function(value){
+    if(value !== this.password){
+        throw new Error('Password missmatch!')
+    }
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
