1a1edb325b9e08505b617a30e7b159eea5536fb4
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-02-29 22:27:45 +0200

    add homeController and setup DB

--- JS BackEnd/exam-preparation-skeleton/src/index.js
+++ JS BackEnd/exam-preparation-skeleton/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 const path = require('path')
 const handlebars = require("express-handlebars");
+const mongoose = require('mongoose')
 
 const PORT = 4000;
 const app = express();
@@ -15,7 +16,9 @@ app.set('views', 'src/views')
 app.use(express.static(path.resolve(__dirname, 'public'))) 
 app.use(express.urlencoded({ extended: false }));
 
-
+mongoose.connect('mongodb://127.0.0.1:27017/petstagram')
+.then(()=> console.log("DB connected succesfully!"))
+.catch(err => console.log("DB error", err.message))
 
 app.use(routes);
 
