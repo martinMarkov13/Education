cd15eeba6bd45e12e951ca5550efef6d034bc229
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-03-06 12:03:49 +0200

    add dynamic navigation

--- JS BackEnd/exam-preparation-skeleton/src/controller/homeController.js
+++ JS BackEnd/exam-preparation-skeleton/src/controller/homeController.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 
 
 router.get("/", (req, res) => {
-    console.log(req.user);
     res.render("home");
 });
 
--- JS BackEnd/exam-preparation-skeleton/src/middlewares/authMiddleware.js
+++ JS BackEnd/exam-preparation-skeleton/src/middlewares/authMiddleware.js
@@ -9,6 +9,8 @@ exports.auth = async (req, res, next) => {
       const decodedToken = await jwt.verify(token, SECRET);
 
       req.user = decodedToken;
+	  res.locals.user = decodedToken;
+	  res.locals.isAuthenticated = true;
 
       next();
     } catch (err) {
@@ -20,3 +22,11 @@ exports.auth = async (req, res, next) => {
     next();
   }
 };
+
+exports.isAuth = async (req, res, next) => {
+  if(!req.user){
+	return res.redirect('/users/login')
+  }
+
+  next()
+}
