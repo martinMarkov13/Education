a73f3645ff5162bce6b73073d21f5ed88c1aa13e
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-03-07 15:14:45 +0200

    Add app skeleton

---
+++ JS BackEnd/petstagram/src/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    unique: [true, "Username already exists!"],
+    minLength: [3, "Username is too short!"]
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required!"],
+  }, 
+  password: {
+    type: String,
+    required: [true, "Password is required!"],
+  },
+});
+
+userSchema.virtual('repeatPassword')
+.set(function(value){
+    if(value !== this.password){
+        throw new Error('Password missmatch!')
+    }
+});
+
+userSchema.pre('save', async function(){
+	const hash = await bcrypt.hash(this.password, 10)
+
+	this.password = hash;
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
