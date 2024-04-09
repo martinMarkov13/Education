d1cad5a81f8c817bc235ea875aa9e80f5185d331
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-04-09 12:19:27 +0300

    Fix error handling

--- JS BackEnd/book-talk/src/controller/bookController.js
+++ JS BackEnd/book-talk/src/controller/bookController.js
@@ -1,46 +1,80 @@
-const router = require("express").Router()
-const bookService = require('../services/bookService')
-const {isAuth} = require('../middlewares/authMiddleware')
-const {getErrorMessage} = require('../utils/errorHelpers')
+const router = require("express").Router();
+const bookService = require("../services/bookService");
+const { isAuth } = require("../middlewares/authMiddleware");
+const { getErrorMessage } = require("../utils/errorHelpers");
 
-router.get('/catalog', async (req, res) => {
-    const books = await bookService.getAll()
-    res.render('books/catalog', { books })
+router.get("/catalog", async (req, res) => {
+  const books = await bookService.getAll();
+  res.render("books/catalog", { books });
+});
+
+router.get("/create", isAuth, (req, res) => {
+  res.render("books/create");
+});
+
+router.post("/create", isAuth, async (req, res) => {
+  const bookData = req.body;
+  const owner = req.user._id;
+
+  try {
+    await bookService.create({ ...bookData, owner });
+    res.redirect("/books/catalog");
+  } catch (err) {
+    res.render("books/create", { error: getErrorMessage(err) });
+  }
+});
+
+router.get("/:bookId/delete", async (req, res) => {
+  try {
+    await bookService.deleteOne(req.params.bookId);
+
+    res.redirect("/books/catalog");
+  } catch (err) {
+
+    res.render("books/details", { book, error: getErrorMessage(err) });
+  }
+});
+
+router.get("/:bookId/details", async (req, res) => {
+  const book = await bookService.getOne(req.params.bookId);
+  const user = req.user?._id;
+
+  const isCreator = book.owner._id == user;
+
+  const isWished = book.wishingLst.toString().includes(user);
+
+  res.render("books/details", { book, isCreator, isWished });
+});
+
+router.get("/:bookId/wish", async (req, res) => {
+  const book = await bookService.getWished(req.params.bookId);
+  const user = req.user._id;
+
+  book.wishingLst.push(user);
+
+  await book.save();
+
+  res.redirect(`/books/${req.params.bookId}/details`);
+});
+
+router.get('/:bookId/edit', async (req, res) => {
+  const book = await bookService.getOne(req.params.bookId);
+
+    res.render('books/edit', {book})
 })
 
-router.get('/create', isAuth, (req, res) => {
-    res.render('books/create')
-})
+router.post('/:bookId/edit', async (req, res) => {
+    const updateData = req.body;
+    const bookId = req.params.bookId;
 
-router.post('/create', isAuth, async (req, res) => {
-    const bookData = req.body;
-    const owner = req.user._id;
+    const book = await bookService.getOne(req.params.bookId);
 
     try{
-        await bookService.create({...bookData, owner})
-        res.redirect('/books/catalog')
+        await bookService.updateBook(bookId, updateData)
+        res.redirect(`/books/${bookId}/details`)
     }catch(err){
-        res.render('books/create', {error: getErrorMessage(err)})
+        res.render('books/edit', {book, error: getErrorMessage(err) })
     }
-
-})
-
-router.get('/:bookId/delete', async (req, res) => {
-    try{
-        const book = await bookService.deleteOne(req.params.bookId)
-        res.redirect('/books/catalog')
-    }catch(err){
-        res.render('books/details', {book, error: getErrorMessage(err)})
-    }
-})
-
-router.get('/:bookId/details', async (req, res) => {
-    const book = await bookService.getOne(req.params.bookId)
-
-    const isCreator = book.owner._id == req.user?._id;
-
-
-    res.render('books/details', {book, isCreator})
 })
 
 module.exports = router;
--- JS BackEnd/book-talk/src/services/bookService.js
+++ JS BackEnd/book-talk/src/services/bookService.js
@@ -7,3 +7,7 @@ exports.create = (bookData) => Book.create(bookData);
 exports.deleteOne = (book) => Book.findByIdAndDelete(book);
 
 exports.getOne = (book) => Book.findById(book).populate('owner').lean()
+
+exports.getWished = (book) => Book.findById(book).populate('owner')
+
+exports.updateBook = (book, updateData) => Book.findByIdAndUpdate(book, updateData, { runValidators: true })
