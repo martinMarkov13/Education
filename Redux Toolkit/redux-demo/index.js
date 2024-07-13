8ac94c7159a5cdf5b54b2b60eb4b9ba501ac76cf
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-07-13 18:50:29 +0300

    redux principles

--- Redux Toolkit/redux-demo/index.js
+++ Redux Toolkit/redux-demo/index.js
@@ -1,1 +1,41 @@
-console.log("From index.js");
+const redux = require('redux')
+const createStore = redux.createStore;
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+// (previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(()=> console.log('Updated state', store.getState()))
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
