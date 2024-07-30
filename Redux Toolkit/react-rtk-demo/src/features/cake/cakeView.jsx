0eae4d43b6d83f6fb1cc3a903ad612e2d8c77126
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-07-31 00:27:07 +0300

    Provider and useSelector

--- Redux Toolkit/react-rtk-demo/src/features/cake/cakeView.jsx
+++ Redux Toolkit/react-rtk-demo/src/features/cake/cakeView.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const CakeView = () => {
+  const numOfCakes = useSelector((state)=> state.cake.numOfCakes)
   return (
     <div>
-        <h2>Number of cakes - </h2>
+        <h2>Number of cakes - {numOfCakes}</h2>
         <button>Order cake</button>
         <button>Restock cakes</button>
     </div>
--- Redux Toolkit/react-rtk-demo/src/features/icecream/icecreamView.jsx
+++ Redux Toolkit/react-rtk-demo/src/features/icecream/icecreamView.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const IcecreamView = () => {
+  const numOfIcecreams = useSelector((state => state.icecream.numOfIcecreams))
   return (
     <div>
-        <h2>Number of ice creams - </h2>
+        <h2>Number of ice creams - {numOfIcecreams}</h2>
         <button>Order ice cream</button>
         <button>Restock ice creams</button>
     </div>
