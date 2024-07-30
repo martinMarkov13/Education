36675b418ef75f4c333d56ccf2d5f5b3077eeb71
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-07-30 22:36:12 +0300

    Implementing middleware

---
+++ Redux Toolkit/redux-toolkit-demo/index.js
@@ -0,0 +1,16 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+
+console.log('Initial state', store.getState());
+const unsubscribe = store.subscribe(() => {})
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(4))
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(3))
+
+unsubscribe()
