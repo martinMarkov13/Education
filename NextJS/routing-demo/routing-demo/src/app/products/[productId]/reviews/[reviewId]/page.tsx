8ae74c1fb27ce5362156ab11cd6b6832c0fc0d5d
martinMarkov13 <123933613+martinMarkov13@users.noreply.github.com>
2024-08-05 14:10:42 +0300

    routing fundamentals

---
+++ NextJS/routing-demo/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ReviewDetail({params}: {
+    params:{
+        productId:string,
+        reviewId: string
+    }
+}) {
+  return (
+    <h1>Review {params.reviewId} for product {params.productId} </h1>
+  )
+}
